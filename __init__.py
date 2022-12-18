12d3bf7e8d0e1b426cc0735e0b7a88bdb03418fb
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2022-12-18 15:11:29 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ def new():
     return "Prefix and Postfix notation are not complete and can only used for 2 integers"
 __name__= "lib.py"
 __doc__= {"__init__": __init__, "__name__":__name__, "new":new, "numlist":numlist, 
-"sym":sym, "lg":lg, "new_math":new_math,"new_math.round":new_math.round,
+"sym":sym, "new_math":new_math,"new_math.round":new_math.round,
 "new_math.cbrt": new_math.cbrt, "new_math.logadd": new_math.logadd, 
 "new_math.logsub": new_math.logsub, "new_math.__init__": new_math.__init__, 
 "matrix": matrix, "matrix.matrix": matrix.matrix, "matrix.mat_trans": matrix.mat_trans, 
