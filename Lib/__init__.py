e5795d13048fe3c47baf4e6971a22fd875209d25
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-03-09 21:42:57 +0530

    Update __init__.py

--- Lib/__init__.py
+++ Lib/__init__.py
@@ -3,15 +3,46 @@ Personal Python Module(Lib)
 These programs are saved in a folder named Lib of user
 """
 from Lib.main import *
+from Lib.shapes import *
 def __init__():
     return "This is a Python file used as a personal python module "
 def new():
     return "Prefix and Postfix notation are now complete"
 __name__= "Lib"
-_version="0.3.9"
-__doc__= {"__init__": __init__, "__name__":__name__, "new":new,"new_math":new_math,
-"new_math.round":new_math.round,"new_math.cbrt": new_math.cbrt, "new_math.logadd": new_math.logadd, 
-"new_math.logsub": new_math.logsub, "new_math.__init__": new_math.__init__, 
-"matrix": matrix, "matrix.matrix": matrix.matrix, "matrix.mat_trans": matrix.mat_trans, 
-"matrix.determin": matrix.determin, "matrix.__init__": matrix.__init__, 
-"notation": notation, "notation.prefix": notation.prefix, "notation.postfix": notation.postfix}
+_version="0.4.4"
+__doc__= {
+    "__init__": __init__, 
+    "__name__":__name__, 
+    "new":new, "new_math":new_math,
+    "new_math.round":new_math.round,
+    "new_math.cbrt": new_math.cbrt, 
+    "new_math.logadd": new_math.logadd, 
+    "new_math.logsub": new_math.logsub, 
+    "new_math.__init__": new_math.__init__, 
+    "matrix": matrix, "matrix.matrix": matrix.matrix, 
+    "matrix.mat_trans": matrix.mat_trans, 
+    "matrix.determin": matrix.determin, 
+    "matrix.__init__": matrix.__init__, 
+    "notation": notation, 
+    "notation.prefix": notation.prefix, 
+    "notation.postfix": notation.postfix,
+    "Cuboid": Cuboid, "Cube": Cube, 
+    "Cylinder": Cylinder, "Sphere": Sphere, 
+    "Cuboid.TotalSurfaceArea": Cuboid.TotalSurfaceArea, 
+    "Cuboid.LateralSurfaceArea": Cuboid.LateralSurfaceArea, 
+    "Cuboid.Volume": Cuboid.Volume, 
+    "Cuboid.lengthofDiagonal": Cuboid.lengthofDiagonal, 
+    "Cuboid.execution": Cuboid.execution, 
+    "Cube.TotalSurfaceArea": Cube.TotalSurfaceArea, 
+    "Cube.LateralSurfaceArea": Cube.LateralSurfaceArea, 
+    "Cube.Volume": Cube.Volume, 
+    "Cube.lengthofDiagonal": Cube.lengthofDiagonal, 
+    "Cube.execution": Cube.execution, 
+    "Cylinder.TotalSurfaceArea": Cylinder.TotalSurfaceArea, 
+    "Cylinder.CurvedSurfaceArea": Cylinder.CurvedSurfaceArea, 
+    "Cylinder.Volume": Cylinder.Volume, 
+    "Cylinder.execution": Cylinder.execution, 
+    "Sphere.SurfaceArea": Sphere.SurfaceArea, 
+    "Sphere.Volume": Sphere.Volume, 
+    "Sphere.execution": Sphere.execution
+}
