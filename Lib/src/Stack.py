e97de3d8db139a0bc96bca83a352df3998b1403c
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-24 15:00:11 +0530

    Update Lib/src/Stack.py

--- Lib/src/Stack.py
+++ Lib/src/Stack.py
@@ -1,6 +1,6 @@
 class Stack:
     def __init__(self, *args):
-        self.list= list()
+        self.list= list(args )
     def push(self, elem):
         self.list.append(elem)
     def pushMany(self, *args):
