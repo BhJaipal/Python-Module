85e72a6617941eb687b1aebac8a39ea99dd47d4e
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-24 15:15:58 +0530

    Update Lib/test/StackTest.py

--- Lib/test/StackTest.py
+++ Lib/test/StackTest.py
@@ -3,4 +3,4 @@ from Lib.Stack import *
 newStack= Stack()
 newStack.push(4)
 newStack.pushMany(3, 4, 2, 7)
-print(f"3 poped elements are: {newStack.po}")
+print(f"3 poped elements are: {newStack.popMany()}")
