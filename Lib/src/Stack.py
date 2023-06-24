356fff5d8cdc97fa09b6dab73f0123ac808485b3
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-24 15:07:51 +0530

    Update Lib/src/Stack.py

--- Lib/src/Stack.py
+++ Lib/src/Stack.py
@@ -17,7 +17,7 @@ class Stack:
         else:
             return self.list.pop()
     def popMany(self, n: int):
-        
+        # 
         popedElemList= []
         for i in range(n):
             popedElemList.append(self.pop())
