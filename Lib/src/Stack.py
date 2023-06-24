91644fe349c08567e29423aa01a5a3305489fde4
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-24 15:08:34 +0530

    Update Lib/src/Stack.py

--- Lib/src/Stack.py
+++ Lib/src/Stack.py
@@ -17,7 +17,7 @@ class Stack:
         else:
             return self.list.pop()
     def popMany(self, n: int):
-        # removing last n elements of Stack from 
+        # removing last n elements of Stack from back
         popedElemList= []
         for i in range(n):
             popedElemList.append(self.pop())
