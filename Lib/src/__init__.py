d6b0c6c6d8702a8023b751a5d43994a1ec39ca6b
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-24 14:44:29 +0530

    Update Lib/src/__init__.py

--- Lib/src/__init__.py
+++ Lib/src/__init__.py
@@ -47,6 +47,6 @@ __doc__= {
     "Sphere.Volume": Sphere.Volume, 
     "Sphere.execution": Sphere.execution,
     "Stack": Stack, "Stack.pop": Stack.pop,
-    "Stack.popMany": Stack.popMany, "Stack.push": Stack.push
+    "Stack.popMany": Stack.popMany, "Stack.push": Stack.push,
     "Stack.pushMany": Stack.pushMany
 }
