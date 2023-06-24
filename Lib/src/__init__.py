a3ceaa900526185400517cf4bb84f63d98a2c82b
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-24 14:39:02 +0530

    Update Lib/src/__init__.py

--- Lib/src/__init__.py
+++ Lib/src/__init__.py
@@ -4,7 +4,7 @@ These programs are saved in a folder named Lib of user
 """
 from Lib.main import *
 from Lib.shapes import *
-from Lib.
+from Lib.St
 def __init__():
     return "This is a Python file used as a personal python module "
 def new():
