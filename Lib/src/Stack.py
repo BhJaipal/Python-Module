f729caedb3d2a9a73cfcbf3491d2aefa4fc46e2d
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-25 11:37:46 +0530

    Update Lib/src/Stack.py

--- Lib/src/Stack.py
+++ Lib/src/Stack.py
@@ -34,7 +34,7 @@ class Stack:
         found = -1
         for i in self.list:
             if i== element:
-                found 
+                found =1
                 return self.list.index(i)
             else:
                 continue
