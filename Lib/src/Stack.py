423cb135515074df045e1de74c420f1fd772ac5c
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-25 11:44:35 +0530

    Update Lib/src/Stack.py

--- Lib/src/Stack.py
+++ Lib/src/Stack.py
@@ -31,7 +31,7 @@ class Stack:
             print(i, end=" ")
         print()
     def search(self, element):
-        # searches for element, if not 
+        # searches for element, if not found 
         found = -1
         for i in self.list:
             if i== element:
