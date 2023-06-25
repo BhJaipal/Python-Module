b9ae4b6673d50a4118f8ccba2759eb59c9423493
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-25 11:41:04 +0530

    Update Lib/src/Stack.py

--- Lib/src/Stack.py
+++ Lib/src/Stack.py
@@ -34,7 +34,7 @@ class Stack:
         found = -1
         for i in self.list:
             if i== element:
-                found = self.
+                found = self.list
                 break
             else:
                 continue
