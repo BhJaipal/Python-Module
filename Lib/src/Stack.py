b8f3e1268a23712c1ff955493d875119015d8b5a
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-25 11:35:53 +0530

    Update Lib/src/Stack.py

--- Lib/src/Stack.py
+++ Lib/src/Stack.py
@@ -31,7 +31,7 @@ class Stack:
             print(i, end=" ")
         print()
     def search(self, element):
-        found = 
+        found = -1
         for i in self.list:
             if i== element:
                 return self.list.index(i)
